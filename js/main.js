d5fe86fa3b37c1bc49452cf1bab70812f58df238
Abjalilov Dior <abjalilovdiyor1@gmail.com>
2021-05-25 23:49:06 +0500

    add img

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-// let menuItem = document.querySelector(".menu_items .item");
-// menuItem.addEventListener("mousemove", (e) => {
-//   let context = document.querySelector(".contextMenu");
-//   context.style.display = "block";
-// });
 let plusBloon = true;
 let plusContext = document.querySelector(".plusIcon");
 plusContext.addEventListener("click", () => {
@@ -14,6 +9,25 @@ plusContext.addEventListener("click", () => {
     document.querySelector(".plusTitle").style.display = "none";
   }
 });
+
+let backg2 = document.querySelectorAll(".background")[1];
+let aBtn2 = document.querySelectorAll(".bg_image .selector .anchor");
+for (let i = 0; i < aBtn2.length; i++) {
+  aBtn2[i].addEventListener("click", (e) => {
+    e.preventDefault();
+    if (i == 0) {
+      aBtn2[1].classList.remove("selected");
+      aBtn2[i].classList.add("selected");
+      backg2.style.left = "0px";
+      backg2.style.width = "100%";
+    } else if (i == 1) {
+      backg2.style.left = "85px";
+      backg2.style.width = "127px";
+      aBtn2[i].classList.add("selected");
+      aBtn2[0].classList.remove("selected");
+    }
+  });
+}
 
 let backg = document.querySelector(".background");
 let aBtn = document.querySelectorAll(".selector .anchor");
@@ -56,3 +70,17 @@ $(".p82 .bar").circleProgress({
 $(".p85 .bar").circleProgress({
   value: 0.85,
 });
+
+//
+// section.inner_content div.column_content div.card.style_2 div.image
+let imgTreler = document.querySelectorAll(".style_2 .image .wrapper");
+for (let j = 0; j < imgTreler.length; j++) {
+  imgTreler[j].addEventListener("mouseover", (e) => {
+    let classLis = e.target.parentElement.classList[1];
+    let aa = String("." + e.target.parentElement.classList[1] + " " + "img");
+    let urlImg = document.querySelectorAll(aa);
+    let urlStr = String(urlImg[j].src);
+    let backgImg = document.querySelector(".bg_image.video");
+    backgImg.style.backgroundImage = "url(" + urlStr + ")";
+  });
+}
