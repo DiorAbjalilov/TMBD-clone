4ecc404f5f3d86ee925aa6717ce2419666183da2
Abjalilov Dior <abjalilovdiyor1@gmail.com>
2021-05-25 14:50:07 +0500

    carusel add filimi

--- js/main.js
+++ js/main.js
@@ -19,8 +19,6 @@ let backg = document.querySelector(".background");
 let aBtn = document.querySelectorAll(".selector .anchor");
 for (let i = 0; i < aBtn.length; i++) {
   aBtn[i].addEventListener("click", (e) => {
-    // console.log(i);
-    let mouse = e.target.parentNode.parentNode;
     if (i == 0) {
       aBtn[1].classList.remove("selected");
       aBtn[i].classList.add("selected");
@@ -35,54 +33,26 @@ for (let i = 0; i < aBtn.length; i++) {
   });
 }
 
-window.onload = function () {
-  var can = document.getElementById("canvas"),
-    spanProcent = document.getElementById("procent"),
-    c = can.getContext("2d");
-
-  var posX = can.width / 2,
-    posY = can.height / 2,
-    fps = 1000 / 200,
-    procent = 0,
-    oneProcent = 360 / 100,
-    result = oneProcent * 77;
-
-  c.lineCap = "round";
-  arcMove();
-
-  function arcMove() {
-    var deegres = 0;
-    var acrInterval = setInterval(function () {
-      deegres += 1;
-      c.clearRect(0, 0, can.width, can.height);
-      procent = deegres / oneProcent;
-
-      spanProcent.innerHTML = procent.toFixed();
-
-      c.beginPath();
-      c.arc(
-        posX,
-        posY,
-        70,
-        (Math.PI / 180) * 270,
-        (Math.PI / 180) * (270 + 360)
-      );
-      c.strokeStyle = "#204529";
-      c.lineWidth = "10";
-      c.stroke();
-
-      c.beginPath();
-      c.strokeStyle = "#21d07a";
-      c.lineWidth = "10";
-      c.arc(
-        posX,
-        posY,
-        70,
-        (Math.PI / 180) * 270,
-        (Math.PI / 180) * (270 + deegres)
-      );
-      c.stroke();
-      if (deegres >= result) clearInterval(acrInterval);
-    }, fps);
-  }
+let options = {
+  startAngle: -1.55,
+  size: 35,
+  value: 0.77,
+  fill: { color: "#21d07a" },
 };
+$(".circle .bar")
+  .circleProgress(options)
+  .on("circle-animation-progress", function (event, progress, stepValue) {
+    $(this)
+      .parent()
+      .find("span")
+      .text(String(stepValue.toFixed(2).substr(2)) + "%");
+  });
+$(".p86 .bar").circleProgress({
+  value: 0.86,
+});
+$(".p82 .bar").circleProgress({
+  value: 0.82,
+});
+$(".p85 .bar").circleProgress({
+  value: 0.85,
+});
