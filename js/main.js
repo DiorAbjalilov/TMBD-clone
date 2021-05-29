59839392c920757948c90038071061f7f33bf307
Abjalilov Dior <abjalilovdiyor1@gmail.com>
2021-05-29 09:10:11 +0500

    img onload

--- js/main.js
+++ js/main.js
@@ -129,6 +129,54 @@ window.addEventListener("scroll", (e) => {
   }
 });
 
+// data img
+const data = [
+  {
+    id: 1,
+    img_url: "../imgs/imgRand/imgRand.jpg",
+  },
+  {
+    id: 2,
+    img_url: "../imgs/imgRand/imgRand1.jpg",
+  },
+  {
+    id: 3,
+    img_url: "../imgs/imgRand/imgRand2.jpg",
+  },
+  {
+    id: 4,
+    img_url: "../imgs/imgRand/imgRand3.jpg",
+  },
+  {
+    id: 5,
+    img_url: "../imgs/imgRand/imgRand4.jpg",
+  },
+  {
+    id: 6,
+    img_url: "../imgs/imgRand/imgRand5.jpg",
+  },
+  {
+    id: 7,
+    img_url: "../imgs/imgRand/imgRand6.jpg",
+  },
+  {
+    id: 8,
+    img_url: "../imgs/imgRand/imgRand7.jpg",
+  },
+  {
+    id: 9,
+    img_url: "../imgs/imgRand/imgRand8.jpg",
+  },
+  {
+    id: 10,
+    img_url: "../imgs/imgRand/imgRand9.jpg",
+  },
+];
+
+let bannerImg = document.querySelector("section.new_index");
+
 window.addEventListener("load", () => {
-  console.log(Math.random() * 10);
+  let rand = Math.floor(Math.random() * 10);
+  let imgs = data[rand].img_url;
+  bannerImg.style.backgroundImage = "url(" + imgs + ")";
 });
