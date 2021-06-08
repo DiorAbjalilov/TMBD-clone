21b77b2b66ff416c18a3803a8bcde554d21c9910
DiorAbjalilov <abjalilovdiyor1@gmail.com>
2021-06-08 19:01:07 +0500

    delet local api

--- js/fetchAPI.js
+++ js/fetchAPI.js
@@ -1,5 +1,11 @@
 const apiKey = "db625edffe159ea93eb6c0e7d83d7089";
 let divBlock = document.querySelector(".column_content.flex.scroller.loaded");
+let options = {
+    startAngle: -1.55,
+    size: 35,
+    // value: 0.77,
+    fill: { color: "#21d07a" },
+  };
 
 let url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
 
@@ -8,9 +14,9 @@ fetch(url)
   .then((movies) => {
     let { results } = movies;
     results.forEach((item) => {
-      let { poster_path, title, original_title, release_date } = item;
+      let { poster_path, title, original_title, release_date, vote_average } = item;
     //   console.log(item);
-      Movies.getMovieImg(poster_path, title, original_title, release_date);
+      Movies.getMovieImg(poster_path, title, original_title, release_date, vote_average);
     });
   })
   .catch(function (err) {
@@ -18,7 +24,7 @@ fetch(url)
   });
 let i = 0;
 class Movies {
-  static getMovieImg(img_id, title, name, dataName) {
+  static getMovieImg(img_id, title, name, dataName, vote_average) {
     fetch(`https://image.tmdb.org/t/p/w500${img_id}`)
       .then((res) => res)
       .then((imges) => {
@@ -47,7 +53,7 @@ class Movies {
                 divContent.innerHTML = `
                 <div class="consensus tight">
                     <div class="outer_ring">
-                        <div class="circle p82">
+                        <div class="circle p${vote_average*10}">
                             <div class="bar"></div>
                             <div class="box"><span></span></div>
                         </div>
@@ -58,10 +64,25 @@ class Movies {
                 </h2>
                 <p>${dataName}</p>
                         `;
-
+                        // console.log(divContent);
+                        this.getProgres(vote_average)
+  
             })
             .catch((err) => {
                 console.log(err, "error comunt");
+            });
+        }
+        static getProgres(vote_average){
+            $(".circle .bar")
+            .circleProgress(options)
+            .on("circle-animation-progress", function (event, progress, stepValue) {
+            $(this)
+                .parent()
+                .find("span")
+                .text(String(stepValue.toFixed(2).substr(2)) + "%");
+            });
+            $(`.p${vote_average*10} .bar`).circleProgress({
+                value: (vote_average/10).toFixed(2)
             });
         }
     }
