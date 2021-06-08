d026dd33a63dd178e103eddff23f9818e9a1796e
DiorAbjalilov <abjalilovdiyor1@gmail.com>
2021-06-09 01:16:26 +0500

    fetch finsh

--- js/fetchAPI.js
+++ js/fetchAPI.js
@@ -147,3 +147,80 @@ class Movies {
   })
 }
 }
+let url1=`https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`;
+let divTrendeng=document.querySelector('.trending .column_content.flex.scroller.loaded')
+fetch(url1)
+.then((res) => res.json())
+.then((movie) => {
+    let {results}=movie
+      results.forEach((item)=>{
+        //   console.log(item);
+          let {poster_path, original_title, release_date}=item;
+          let {vote_average}=item;
+          MovieTrend.getProgres(vote_average);
+          MovieTrend.getMovieTrenImg(poster_path, original_title, release_date, vote_average)
+      })
+}).catch((err)=>{
+    console.log(err, 'lof error')
+})
+
+class MovieTrend{
+    static getMovieTrenImg(poster_path, original_title, release_date, vote_average){
+        fetch(`https://image.tmdb.org/t/p/w500${poster_path}`)
+            .then((res) => res)
+            .then((imges) => {
+                let { url } = imges;
+                let trendCard=document.createElement('div');
+                let trendCardImg=document.createElement('div');
+                let trendCardCont=document.createElement('div');
+                trendCard.className='card style_1';
+                trendCardImg.className='image';
+                trendCardCont.className='content';
+                divTrendeng.appendChild(trendCard);
+                trendCard.appendChild(trendCardImg);
+                trendCard.appendChild(trendCardCont);
+                trendCardImg.innerHTML=`
+                    <div class="wrapper">
+                        <a href="#" class="image">
+                            <img class="poster" src="${url}" alt="">
+                        </a>
+                    </div>
+                    <div class="options">
+                        <a href="#" class="no_click">
+                            <div class="glyphicons_v2 circle-more white"></div>
+                        </a>
+                    </div>
+                `
+                trendCardCont.innerHTML=`
+                <div class="consensus tight">
+                    <div class="outer_ring">
+                        <div class="circle p${vote_average*10}">
+                            <div class="bar"></div>
+                            <div class="box"><span></span></div>
+                        </div>
+                    </div>
+                </div>
+                <h2>
+                    <a href="#">${original_title}</a>
+                </h2>
+                <p>
+                    ${release_date}
+                </p>
+                `;
+            })
+            // this.getProgres(vote_average);
+        }
+        static getProgres(vote_average) {
+            $(".circle .bar")
+            .circleProgress(options)
+            .on("circle-animation-progress", function (event, progress, stepValue) {
+                $(this)
+                .parent()
+                .find("span")
+                .text(String(stepValue.toFixed(2).substr(2)) + "%");
+            });
+            $(`.p${vote_average * 10} .bar`).circleProgress({
+                value: (vote_average / 10).toFixed(2),
+            });
+        }
+    }
